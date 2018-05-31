ceb182f082310a9a4366b8f1503e3faeaddbbf1c
Sijmen J. Mulder <ik@sjmulder.nl>
2018-05-31 21:41:44 +0200

    Do without getline()

--- nycat.c
+++ nycat.c
@@ -1,4 +1,3 @@
-#define _WITH_GETLINE
 #define LEN(a) (sizeof(a)/sizeof(*(a)))
 
 #include <stdio.h>
@@ -15,14 +14,34 @@ static const char colors[][6] = {
 	"\x1B[95m"
 };
 
+static void
+nywrite(const char *p, size_t n, FILE *f)
+{
+	static int color = 0;
+
+	const char *cur = p;
+	const char *lf;
+
+	fputs(colors[color], stdout);
+
+	while ((lf = memchr(cur, '\n', p+n-cur)) != NULL) {
+		fwrite(cur, 1, lf-cur+1, f);
+		color = (color+1) % (int)LEN(colors);
+		fputs(colors[color], f);
+		cur = lf+1;
+	}
+
+	fwrite(cur, 1, p+n-cur, f);
+	fputs("\x1B[0m", f);
+}
+
 int
 main(int argc, char **argv)
 {
 	char **path;
-	int colori = 0;
 	FILE *f;
-	char *line = NULL;
-	size_t cap = 0;
+	char buf[4096];
+	size_t n;
 
 	if (argc <= 1) {
 		fputs("usage: nycat file file ...\n", stderr);
@@ -32,24 +51,16 @@ main(int argc, char **argv)
 	for (path = &argv[1]; *path; path++) {
 		if (!(f = fopen(*path, "r")))
 			goto error;
-
-		while (getline(&line, &cap, f) != -1) {
-			fputs(colors[colori], stdout);
-			colori = (colori + 1) % (int)LEN(colors);
-			fputs(line, stdout);
-		}
-
+		while ((n = fread(buf, 1, sizeof(buf), f)) > 0)
+			nywrite(buf, n, stdout);
 		if (ferror(f))
 			goto error;
-
 		fclose(f);
 	}
 
-	fputs("\x1B[0m", stdout);
 	return 0;
 
 error:
-	fputs("\x1B[0m", stdout);
 	fflush(stdout);
 	fprintf(stderr, "%s: %s\n", strerror(errno), *path);
 	return 1;
